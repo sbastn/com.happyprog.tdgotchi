788fba77925008027dc6d6a1909c83914ab7b86a
Sebastian Hermida <sebastian@sbastn.com>
2011-04-02 18:28:22 -0400

    adding hook to the JUnitCore listener

--- src/com/happyprog/tdgotchi/subscriber/JUnitTestSubscriber.java
+++ src/com/happyprog/tdgotchi/subscriber/JUnitTestSubscriber.java
@@ -1,21 +1,33 @@
 package com.happyprog.tdgotchi.subscriber;
 
+import org.eclipse.jdt.junit.JUnitCore;
+import org.eclipse.jdt.junit.TestRunListener;
+import org.eclipse.jdt.junit.model.ITestElement;
+import org.eclipse.jdt.junit.model.ITestElement.Result;
+import org.eclipse.jdt.junit.model.ITestRunSession;
 
-public class JUnitTestSubscriber implements TestSubscriber {
+public class JUnitTestSubscriber extends TestRunListener implements TestSubscriber {
 
 	private TestObserver observer;
 
 	@Override
 	public void subscribe(TestObserver observer) {
 		this.observer = observer;
+		addAsTestListener();
 	}
 
-	public void onPassingTest() {
-		observer.onPassingTest();
+	void addAsTestListener() {
+		JUnitCore.addTestRunListener(this);
 	}
 
-	public void onFailingTest() {
-		observer.onFailingTest();
+	@Override
+	public void sessionFinished(ITestRunSession session) {
+		Result testResult = session.getTestResult(true);
+		if (testResult.equals(ITestElement.Result.OK)) {
+			observer.onPassingTest();
+		} else {
+			observer.onFailingTest();
+		}
 	}
 
 }
--- test/com/happyprog/tdgotchi/subscriber/JUnitTestSubscriberTest.java
+++ test/com/happyprog/tdgotchi/subscriber/JUnitTestSubscriberTest.java
@@ -2,37 +2,44 @@ package com.happyprog.tdgotchi.subscriber;
 
 import static org.mockito.Mockito.*;
 
+import org.eclipse.jdt.junit.model.ITestElement;
+import org.eclipse.jdt.junit.model.ITestRunSession;
 import org.junit.Before;
 import org.junit.Test;
-
-import com.happyprog.tdgotchi.subscriber.JUnitTestSubscriber;
-import com.happyprog.tdgotchi.subscriber.TestObserver;
-
 
 public class JUnitTestSubscriberTest {
 
 	private TestObserver observer;
 	private JUnitTestSubscriber subscriber;
+	private ITestRunSession session;
 
 	@Before
 	public void before() {
+		session = mock(ITestRunSession.class);
 		observer = mock(TestObserver.class);
-		subscriber = new JUnitTestSubscriber();
+
+		subscriber = spy(new JUnitTestSubscriber());
+		doNothing().when(subscriber).addAsTestListener();
 
 		subscriber.subscribe(observer);
 	}
 
 	@Test
 	public void onPassingTest_updateObserver() throws Exception {
-		subscriber.onPassingTest();
+		when(session.getTestResult(true)).thenReturn(ITestElement.Result.OK);
+
+		subscriber.sessionFinished(session);
 
 		verify(observer).onPassingTest();
 	}
 
 	@Test
 	public void onFailingTest_updateObserver() throws Exception {
-		subscriber.onFailingTest();
+		when(session.getTestResult(true)).thenReturn(ITestElement.Result.FAILURE);
+
+		subscriber.sessionFinished(session);
 
 		verify(observer).onFailingTest();
 	}
+
 }
