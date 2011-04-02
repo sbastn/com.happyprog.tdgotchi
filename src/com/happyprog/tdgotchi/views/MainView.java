d797af8d5be446299844f6a22fee0cd71c8daf8a
Sebastian Hermida <sebastian@sbastn.com>
2011-04-02 10:20:09 -0400

    App skeleton

---
+++ src/com/happyprog/tdgotchi/views/MainView.java
@@ -0,0 +1,20 @@
+package com.happyprog.tdgotchi.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MainView extends ViewPart {
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
